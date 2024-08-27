01e6e5b759dc6398b40b5d98cd6bade472475abb
Ryvo Octaviano <33638455+octavianor@users.noreply.github.com>
2024-08-27 15:12:58 +0200

    1st version hydrogen storage model (#4)
    * 1st version hydrogen storage model
    
    * clean unit test

--- unit_test/model_example/test_model_example.py
+++ unit_test/model_example/test_model_example.py
@@ -2,9 +2,9 @@ import unittest
 from pollux_model.model_example.model_example import Model1
 
 
-class TestESP(unittest.TestCase):
+class TestModel(unittest.TestCase):
 
-    def test_calculate_bottomhole(self):
+    def test_calculate_model(self):
         # ARRANGE
         param = dict()
         param['parameter_key_1'] = 'parameter_value_1'
@@ -12,23 +12,14 @@ class TestESP(unittest.TestCase):
         model_instance = Model1()
         model_instance.update_parameters(param)
 
-        # define the state IF NEEDED
-        x = []
-
         # define the input values
         u = dict()
         u['input_key_1'] = 0.999
 
         # calculate  output
-        model_instance.calculate_output(u, x)
+        model_instance.calculate_output(u)
 
         # get output
         y = model_instance.get_output()
 
         self.assertAlmostEqual(y['output_key_1'], 1, delta=0.1)
-        print(y['output_key_1'])
-
-
-# Run the unittest from this file
-if __name__ == '__main__':
-    unittest.main()
