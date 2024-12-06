f340916158029bd2605b385fe5dc16961f0a13d7
Egberts <Paul.Egberts@tno.nl>
2024-12-06 14:30:58 +0100

    heat demand component

---
+++ pollux_model/heat_demand/heat_demand.py
@@ -0,0 +1,22 @@
+from pollux_model.model_abstract import Model
+
+
+class HeatDemand(Model):
+    def __init__(self, time_function):
+        super().__init__()
+        self.time_function = time_function
+        self.current_time = 0
+
+    def initialize_state(self, x):
+        """ generate an initial state based on user parameters
+            """
+        pass
+
+    def calculate_output(self):
+        self.output['heat_demand'] = self.time_function(self.current_time)
+
+    def set_time(self, time):
+        self.current_time = time
+
+    def update_time(self, time_step):
+        self.current_time += time_step
